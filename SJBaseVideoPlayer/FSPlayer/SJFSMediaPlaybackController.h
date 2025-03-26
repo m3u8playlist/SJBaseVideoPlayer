0502ae45647169004eee46ee3a1bd11a9fa2541a
m3u8playlist <44310582+m3u8playlist@users.noreply.github.com>
2025-03-26 09:23:40 +0800

    Update SJFSMediaPlaybackController.h

--- SJBaseVideoPlayer/FSPlayer/SJFSMediaPlaybackController.h
+++ SJBaseVideoPlayer/FSPlayer/SJFSMediaPlaybackController.h
@@ -1,1 +1,14 @@
+#import <Foundation/Foundation.h>
+#import "SJMediaPlaybackController.h"
+#import "SJFSMediaPlayer.h"
+@class FSOptions;
 
+NS_ASSUME_NONNULL_BEGIN
+@interface SJFSMediaPlaybackController : SJMediaPlaybackController
+
+@property (nonatomic, strong, null_resettable) FSOptions *options;
+
+@property (nonatomic, strong, readonly, nullable) SJFSMediaPlayer *currentPlayer;
+
+@end
+NS_ASSUME_NONNULL_END
