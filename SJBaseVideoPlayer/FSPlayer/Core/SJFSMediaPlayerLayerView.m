95e42a102919b3ac76b5f8b4c86355d19d84aeff
m3u8playlist <44310582+m3u8playlist@users.noreply.github.com>
2025-03-26 09:41:03 +0800

    Create SJFSMediaPlayerLayerView.m

---
+++ SJBaseVideoPlayer/FSPlayer/Core/SJFSMediaPlayerLayerView.m
@@ -0,0 +1,39 @@
+#import "SJFSMediaPlayerLayerView.h"
+
+@implementation SJFSMediaPlayerLayerView
+- (instancetype)initWithPlayer:(SJFSMediaPlayer *)player {
+    self = [super init];
+    if ( self ) {
+        _player = player;
+        [self addSubview:_player.view];
+        [self setVideoGravity:AVVideoScalingModeResizeAspect];
+        [player addObserver:self forKeyPath:@"firstVideoFrameRendered" options:NSKeyValueObservingOptionNew context:NULL];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    _player.view.frame = self.bounds;
+}
+
+- (void)dealloc {
+    [_player removeObserver:self forKeyPath:@"firstVideoFrameRendered"];
+}
+
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey,id> *)change context:(nullable void *)context {
+    [NSNotificationCenter.defaultCenter postNotificationName:SJMediaPlayerViewReadyForDisplayNotification object:self];
+}
+
+- (void)setVideoGravity:(SJVideoGravity)videoGravity {
+    _player.videoGravity = videoGravity;
+}
+
+- (SJVideoGravity)videoGravity {
+    return _player.videoGravity;
+}
+
+- (BOOL)isReadyForDisplay {
+    return _player.firstVideoFrameRendered;
+}
+@end
