e44d7d87b48a86fc0be439609864ad18feab0c12
m3u8playlist <44310582+m3u8playlist@users.noreply.github.com>
2025-03-26 09:30:22 +0800

    Create SJFSMediaPlayer.h

---
+++ SJBaseVideoPlayer/FSPlayer/Core/SJFSMediaPlayer.h
@@ -0,0 +1,22 @@
+#import "SJMediaPlaybackController.h"
+@class FSOptions;
+
+NS_ASSUME_NONNULL_BEGIN
+FOUNDATION_EXTERN NSErrorDomain const SJFSMediaPlayerErrorDomain;
+
+@interface SJFSMediaPlayer : NSObject<SJMediaPlayer>
+- (instancetype)initWithURL:(NSURL *)URL startPosition:(NSTimeInterval)startPosition options:(IJKFFOptions *)ops;
+
+@property (nonatomic, readonly, strong) NSURL *URL;
+
+@property (nonatomic) NSTimeInterval trialEndPosition;
+
+@property (nonatomic) BOOL pauseWhenAppDidEnterBackground;
+
+@property (nonatomic, readonly) BOOL firstVideoFrameRendered;
+
+@property (nonatomic, readonly) UIView *view;
+
+@property (nonatomic) SJVideoGravity videoGravity;
+@end
+NS_ASSUME_NONNULL_END
